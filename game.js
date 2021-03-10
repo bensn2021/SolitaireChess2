2d8eff9cc0fa298f004a80ac4791e191bcd57a1a
bensn2021 <80428204+bensn2021@users.noreply.github.com>
2021-03-10 15:46:26 -0600

    Initial commit

---
+++ game.js
@@ -0,0 +1,280 @@
+var Switch1 = 1
+var Switch2 = false
+var StartSquare = ""
+var EndSquare = ""
+var StartPiece = ""
+var EndPiece = ""
+var CapturedPiece = 1
+
+function A1() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("A1")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("A1")
+    swap()
+  }
+  Switch2 = false
+}
+
+function A2() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("A2")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("A2")
+    swap()
+  }
+  Switch2 = false
+}
+
+function A3() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("A3")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("A3")
+    swap()
+  }
+  Switch2 = false
+}
+
+function A4() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("A4")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("A4")
+    swap()
+  }
+  Switch2 = false
+}
+
+function B1() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("B1")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("B1")
+    swap()
+  }
+  Switch2 = false
+}
+
+function B2() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("B2")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("B2")
+    swap()
+  }
+  Switch2 = false
+}
+
+function B3() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("B3")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("B3")
+    swap()
+  }
+  Switch2 = false
+}
+
+function B4() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("B4")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("B4")
+    swap()
+  }
+  Switch2 = false
+}
+
+function C1() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("C1")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("C1")
+    swap()
+  }
+  Switch2 = false
+}
+
+function C2() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("C2")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("C2")
+    swap()
+  }
+  Switch2 = false
+}
+
+function C3() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("C3")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("C3")
+    swap()
+  }
+  Switch2 = false
+}
+
+function C4() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("C4")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("C4")
+    swap()
+  }
+  Switch2 = false
+}
+
+function D1() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("D1")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("D1")
+    swap()
+  }
+  Switch2 = false
+}
+
+function D2() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("D2")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("D2")
+    swap()
+  }
+  Switch2 = false
+}
+
+function D3() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("D3")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("D3")
+    swap()
+  }
+  Switch2 = false
+}
+
+function D4() {
+  if(Switch1 == 1 && Switch2 == false) {
+    Switch1 = 2
+    Switch2 = true
+    StartSquare = document.getElementById("D4")
+    StartSquare.style.backgroundColor = "rgba(128,255,128,0.5)"
+  }
+  if(Switch1 == 2 && Switch2 == false) {
+    Switch1 = 1
+    Switch2 = true
+    EndSquare = document.getElementById("D4")
+    swap()
+  }
+  Switch2 = false
+}
+
+function swap() {
+  StartPiece = StartSquare.innerHTML
+  EndPiece = EndSquare.innerHTML
+  if(StartSquare != EndSquare) {
+  if (StartPiece != "") {
+    if(EndPiece == "") {
+      EndSquare.innerHTML = StartPiece
+      StartSquare.innerHTML = ""
+    } else {
+      EndSquare.innerHTML = StartPiece
+      StartSquare.innerHTML = ""
+    }
+  }
+}
+  StartSquare.style.backgroundColor = "rgba(255,255,255,0)"
+  }
